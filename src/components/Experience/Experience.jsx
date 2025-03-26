87fc2471e0f21182e37c07a77b116dd85665977d
mshadhir <mshadhir3@gmail.com>
2025-03-27 00:05:03 +1100

    fixed exp css

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from "./experience.module.css";
+import styles from "./Experience.module.css";
 import skills from "../../data/skills.json";
 import history from "../../data/history.json";
 import { getImageUrl } from '../../utills';
