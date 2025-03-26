688f442d0a03ef01a5fde02851dad8720bd8b0dc
mshadhir <mshadhir3@gmail.com>
2025-03-26 23:44:54 +1100

    fixed image issue

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -14,7 +14,7 @@ export const Hero = () => {
             </p>
             <a href="mailto:mshadhir3@gmail.com" className={styles.contactBtn}>Contact Me</a>
         </div>
-       <img src={getImageUrl("hero/heroImage.png")} alt="Hero Img of me" className={styles.heroImg} />
+       <img src={getImageUrl("hero/heroImage.PNG")} alt="Hero Img of me" className={styles.heroImg} />
         
         <div className={styles.topBlur}></div>
         <div className={styles.bottomBlur}></div>
